b03b506453946985304a1fd962a8e4c488c6d0a2
MlamboTshepang <tshepangmlambo@gmail.com>
2021-07-24 10:25:17 +0200

    initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button AddBtn = (Button) findViewById(R.id.button);
+
+    }
+}
